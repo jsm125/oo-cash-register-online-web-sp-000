f8686bd044572372da97129095dde6a610115b6d
Matt Im <matthew.seiim@gmail.com>
2020-05-10 21:53:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,8 +20,7 @@ attr_accessor :total, :discount, :price, :items
   
   def apply_discount
     if @discount > 0
-      @total = (price * (1-number_to_percentage(@discount))
-    end
+      @total = (price * (1-number_to_percentage(@discount)))
     end
   end
   
