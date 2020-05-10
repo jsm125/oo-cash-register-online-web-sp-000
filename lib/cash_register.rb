6b30376115a737907c73f0601b03067af20bf8dc
Matt Im <matthew.seiim@gmail.com>
2020-05-10 22:11:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister 
   
-attr_accessor :total, :discount, :price, :items
+attr_accessor :total, :discount, :price, :items, :void_last_transaction
   
   def initialize (discount = 0)
     @total = 0
